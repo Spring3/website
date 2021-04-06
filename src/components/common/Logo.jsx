11b312d8de462a88e2d3ec6707d550d8ff72cf3d
Daniel Vasylenko <danielspr3@gmail.com>
2021-04-06 03:48:42 +0200

    feat: render static svg image

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -1,24 +1,8 @@
-import React, { memo, useEffect, useState } from 'react';
+import React, { memo } from 'react';
 import Logo from '../../assets/logo.svg';
 
-const useLogoAnimation = (isReady) => {
-  if (!isReady) {
-    return null;
-  }
-};
-
-const LogoImage = memo(({ width = '80px', height = '40px' }) => {
-  const [isReady, setReady] = useState(false);
-  useEffect(() => {
-    setReady(true);
-  }, []);
-
-  useLogoAnimation(isReady);
-  return (
-    <>
-      <Logo width={width} height={height} fill="white" />
-    </>
-  );
-});
+const LogoImage = memo(({ width = '80px', height = '40px' }) => (
+  <Logo width={width} height={height} fill="white" />
+));
 
 export { LogoImage as Logo };
